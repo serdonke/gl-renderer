a47508534473e7a06d13763dee8fcc2a38906fc8
serdonke <github@serdonke.xyz>
2025-02-25 20:02:07 +0530

    remove inline shader source strings

--- src/main.h
+++ src/main.h
@@ -9,30 +9,5 @@
 #define WINDOW_HEIGHT 600
 #define WINDOW_WIDTH  800
 
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
-    "out vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos, 1.0);\n"
-    "   ourColor = aColor;\n"
-    "}\0";
-
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
-    "}\0";
-
-const char *yellowFragShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n\n"
-    "in vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(ourColor, 1.0);\n"
-    "}\0";
-
 void framebufferResizeCallback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
